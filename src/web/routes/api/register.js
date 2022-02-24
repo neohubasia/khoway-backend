a83140a4bd4fdd495f66f77039cec1b97188bf49
Min Zin Phyo <minzinphyo143@gmail.com>
2022-02-25 00:38:51 +0630

    add chart data with real data

--- src/web/routes/api/register.js
+++ src/web/routes/api/register.js
@@ -1,6 +1,7 @@
 const RegisterModel = require("../../../../database/mongodb/models/register");
 const { handleError } = require("../error_handler");
 const md5 = require("md5");
+const moment = require('moment');
 const utils = require("../../../../utilities/utilities");
 const registers = (module.exports = {});
 
@@ -42,8 +43,10 @@ registers.login = (req, res, next) => {
 };
 
 registers.getChartData = (req, res, next) => {
+  let start_date = moment(req.query.start_date, 'DD/MM/YYYY', true).format();
+  let end_date = moment(req.query.end_date, 'DD/MM/YYYY', true).format();
   RegisterModel.aggregate([
-    { $match: {created_at: { $gte:new Date("2021-01-01T00:00:00.000Z"), $lte: new Date("2022-02-30T00:00:00.000Z")}}},
+    { $match: {created_at: { $gte:new Date(start_date), $lte: new Date(end_date)}}},
     { $group: {_id: {
         "month": { $month: "$created_at" }, 
         "year": { $year: "$created_at" } ,
@@ -51,9 +54,12 @@ registers.getChartData = (req, res, next) => {
       }, 
       "count": { $sum: 1 }
     }},
-    { $sort : { "_id.day":1} }
+    { $sort : { "_id.day":1, "_id.month": 1, "_id.year": 1 } }
   ])
   .then((data) => {
+    data.map(result => {
+      result.labeldata = result._id.year + "/" + result._id.month + "/" + result._id.day;
+    });
     const handler_response = utils.isEmptyArray(data)
       ? { status: "FAIL", data: [] }
       : { status: "SUCCESS", data: data };
