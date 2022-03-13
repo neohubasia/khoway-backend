d0122326613855ed4ac6e02212ae39c4c07e34a0
Min Zin Phyo <minzinphyo143@gmail.com>
2022-03-13 11:19:23 +0630

    add error handler

--- app.js
+++ app.js
@@ -101,7 +101,7 @@ passport.deserializeUser(UserModel.deserializeUser());
 require("./utilities/socket/web-socket");
 
 // error handler
-/*app.use(function (err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get("env") === "development" ? err : {};
@@ -109,6 +109,6 @@ require("./utilities/socket/web-socket");
   // render the error page
   res.status(err.status || 500);
   res.sendFile("./views/404/index.html", { root: __dirname });
-});*/
+});
 
 module.exports = app;
